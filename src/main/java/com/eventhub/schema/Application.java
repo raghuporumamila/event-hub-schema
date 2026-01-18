d322d75e30b2389f671091ce3e99cfd5b158564f
Suneetha Porumamilla <suneetha.porumamilla@MacBook-Air-3.local>
2026-01-18 18:20:18 -0500

    Changes

--- src/main/java/com/eventhub/schema/Application.java
+++ src/main/java/com/eventhub/schema/Application.java
@@ -4,15 +4,16 @@ import java.util.Collections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-		/*SpringApplication app = new SpringApplication(Application.class);
+		//SpringApplication.run(Application.class, args);
+		SpringApplication app = new SpringApplication(Application.class);
 
         app.setDefaultProperties(Collections
-                .singletonMap("server.port", "8083"));
-              app.run(args);*/
+                .singletonMap("server.port", "8082"));
+              app.run(args);
     }
 }
--- src/main/java/com/eventhub/schema/controller/ValidateController.java
+++ src/main/java/com/eventhub/schema/controller/ValidateController.java
@@ -1,11 +1,10 @@
 package com.eventhub.schema.controller;
 
-import com.eventhub.schema.model.ValidateJsonData;
+import com.eventhub.model.ValidateJsonData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import com.eventhub.schema.validator.JSONSchemaValidator;
